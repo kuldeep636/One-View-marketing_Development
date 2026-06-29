9a954eb548a88183071c4f6dc404c094ea25dca0
KULDEEP PAL <palkuldeep4894@gmail.com>
2026-06-29 23:05:17 +0530

    Create 8_Budget_Upload.py

---
+++ pages/8_Budget_Upload.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Budget Upload",
+    page_icon="🎯",
+    layout="wide"
+)
+
+st.title("🎯 Budget Upload")
+
+st.info(
+    "Coming Soon..."
+)
